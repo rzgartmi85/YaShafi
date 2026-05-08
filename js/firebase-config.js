37ad010cd4849a440fc339eda8ad29639525d4eb
rzgartmi85 <55788323+rzgartmi85@users.noreply.github.com>
2026-05-08 03:19:04 +0300

    Add files via upload

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCXJfLwtrI7em4Luf_zLpvXMhSyt79KhCk",
+  authDomain: "yahakeemrisale-59cb2.firebaseapp.com",
+  projectId: "yahakeemrisale-59cb2",
+  storageBucket: "yahakeemrisale-59cb2.firebasestorage.app",
+  messagingSenderId: "832235504266",
+  appId: "1:832235504266:web:82e5e990e1151cdda1a373"
+};
